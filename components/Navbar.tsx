033465389f4fce629583b8c5bbbae0e1ce3dd41f
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2024-09-18 20:38:48 -0300

    feat: navbar browser

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
+import MobileNav from './Mobilenav';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="Yoom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Yoom
+        </p>
+
+        <div className="flex-between gap-5">
+          {/* Clerk - User Management */}
+
+          <MobileNav />
+        </div>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
