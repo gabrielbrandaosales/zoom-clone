367191adcdb4e3136d03780090053c273e28e299
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2024-09-17 22:46:29 -0300

    feat: sidebar finished

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,9 +6,11 @@ import { sidebarLinks } from '@/constants';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 
 const Sidebar = () => {
   const pathname = usePathname();
+
   return (
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex- flex-col gap-6">
@@ -23,7 +25,15 @@ const Sidebar = () => {
                 'flex gap-4 items-center p-4 rounded-lg justify-start',
                 { 'bg-blue-1': isActive },
               )}>
-              {link.label}
+              <Image
+                src={link.imgUrl}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-lg font-semibold max-lg:hidden">
+                {link.label}
+              </p>
             </Link>
           );
         })}
