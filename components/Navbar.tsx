5852f92a8df2019a4cb9d68e6e9c49e92ee5b6b8
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2024-09-30 22:10:48 -0300

    fix: justify content

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,11 @@ import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
 import MobileNav from './MobileNav';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 const Navbar = () => {
   return (
-    <nav className="flex fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+    <nav className="flex fixed justify-between z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
@@ -20,7 +21,9 @@ const Navbar = () => {
       </Link>
 
       <div className="flex-between gap-5">
-        {/* Clerk - User Management */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
 
         <MobileNav />
       </div>
