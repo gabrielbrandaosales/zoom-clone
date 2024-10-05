b1b2a5e37e33206c658699c8152129c991075679
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2024-10-05 18:43:24 -0300

    feat: add date & minute live

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 const Home = () => {
+  const now = new Date();
+
+  const time = now.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+  });
+  const date = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(
+    now,
+  );
+
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
@@ -9,10 +20,8 @@ const Home = () => {
             Upcoming Meeting at: 12:30 PM
           </h2>
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl ">11:30 AM</h1>
-            <p className="text-lg font-medium text-sky-1 lg:text-2xl">
-              Saturday, March 23, 2024
-            </p>
+            <h1 className="text-4xl font-extrabold lg:text-7xl ">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
       </div>
