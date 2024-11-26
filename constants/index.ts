e8fb0cdd82ddafff88ca1e8daa341f57c6dc9a8a
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2024-11-25 21:08:26 -0300

    fix: images constants

--- constants/index.ts
+++ constants/index.ts
@@ -29,7 +29,7 @@ export const sidebarLinks = [
 export const avatarImages = [
   '/images/avatar-1.jpeg',
   '/images/avatar-2.jpeg',
-  '/images/avatar-3.jpeg',
-  '/images/avatar-4.jpeg',
-  '/images/avatar-5.jpeg',
+  '/images/avatar-3.png',
+  '/images/avatar-4.png',
+  '/images/avatar-5.png',
 ];
