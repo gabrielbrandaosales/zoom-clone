6b809f48ed1ba1c68fc03b1f098107a6f1c52695
Gabriel Brandao <gabrielbrandaosales@hotmail.com>
2024-12-09 19:30:34 -0300

    feat: meeting join

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -9,6 +9,7 @@ import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 import { Textarea } from './ui/textarea';
 import ReactDatePicker from 'react-datepicker';
 import { toast } from '@/hooks/use-toast';
+import { Input } from './ui/input';
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -145,6 +146,19 @@ const MeetingTypeList = () => {
         buttonText="Start Meeting"
         handleClick={createMeeting}
       />
+      <MeetingModal
+        isOpen={meetingState === 'isJoiningMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Type the link here"
+        className="text-center"
+        buttonText="Join Meeting"
+        handleClick={() => router.push(values.link)}>
+        <Input
+          placeholder="Meeting Link"
+          className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+        />
+      </MeetingModal>
     </section>
   );
 };
